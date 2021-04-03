16b724a1fe9e396de91019265f744ea5d77113ff
zxl-0213 <287907757@qq.com>
2021-04-03 14:57:28 +0800

    杨辉三角

---
+++ text.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> vv;
+        vv.resize(numRows);
+        for(size_t i=0;i<numRows;++i)
+        {
+            vv[i].resize(i+1);
+            vv[i][0]=1;
+            vv[i][vv[i].size()-1]=1;
+        }
+        for(size_t i=0;i<numRows;++i)
+        {
+            for(size_t j=0;j<vv[i].size();++j)
+            {
+                if(vv[i][j]!=1)
+                {
+                    vv[i][j]=vv[i-1][j-1]+vv[i-1][j];
+                }
+            }
+        }
+        return vv;
+    }
+};
